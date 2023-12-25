255fe9edc443e31180fea2d04cb2b0503f72c168
sancheznot <sancheznotp@gmail.com>
2023-12-25 14:11:26 -0800

    fix metaview

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,24 @@
 import "./globals.css";
 import Provider from "@/components/Providers";
 import Nav from "@/components/MainPage/Nav/Nav";
+import Head from "next/head";
 
 export const metadata = {
   title: "Photo gallery",
   description: "Photo gallery with nextjs and tailwindcss",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
+
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-dvh">
+      
       <body className="w-full h-full bg-gradient-to-tl from-[#e9f4f9] dark:from-[#072B4A] dark:via-[#031f33] via-[#E4EEF2] to-[#FFFFFF] dark:to-[#000000]">
         <Provider>
           <Nav />
--- src/components/NextTheme/ThemeSwitcher.js
+++ src/components/NextTheme/ThemeSwitcher.js
@@ -46,7 +46,7 @@ export function ThemeSwitcher() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6">
+            className="w-6 h-6 sm:w-5 sm:h-5">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
